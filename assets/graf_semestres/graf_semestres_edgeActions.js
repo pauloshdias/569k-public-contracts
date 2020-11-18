bac25d66ce74b85b1966d2e5934a0b1076619adf
Paulo Dias <pauloshdias@gmail.com>
2020-11-18 12:31:23 +0000

    initial commit

---
+++ assets/graf_semestres/graf_semestres_edgeActions.js
@@ -0,0 +1,260 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_AD_2012}","mouseover",function(sym,e){sym.getSymbol("bot_AD_2012").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_AD_2012}","mouseout",function(sym,e){sym.getSymbol("bot_AD_2012").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_CP_2012}","mouseover",function(sym,e){sym.getSymbol("bot_CP_2012").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_CP_2012}","mouseout",function(sym,e){sym.getSymbol("bot_CP_2012").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_O_2012}","mouseover",function(sym,e){sym.getSymbol("bot_O_2012").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_O_2012}","mouseout",function(sym,e){sym.getSymbol("bot_O_2012").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_AD_2013}","mouseover",function(sym,e){sym.getSymbol("bot_AD_2013").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_AD_2013}","mouseout",function(sym,e){sym.getSymbol("bot_AD_2013").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_CP_2013}","mouseover",function(sym,e){sym.getSymbol("bot_CP_2013").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_CP_2013}","mouseout",function(sym,e){sym.getSymbol("bot_CP_2013").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_O_2013}","mouseover",function(sym,e){sym.getSymbol("bot_O_2013").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_O_2013}","mouseout",function(sym,e){sym.getSymbol("bot_O_2013").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_AD_2014}","mouseover",function(sym,e){sym.getSymbol("bot_AD_2014").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_AD_2014}","mouseout",function(sym,e){sym.getSymbol("bot_AD_2014").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_CP_2014}","mouseover",function(sym,e){sym.getSymbol("bot_CP_2014").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_CP_2014}","mouseout",function(sym,e){sym.getSymbol("bot_CP_2014").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_O_2014}","mouseover",function(sym,e){sym.getSymbol("bot_O_2014").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_O_2014}","mouseout",function(sym,e){sym.getSymbol("bot_O_2014").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_AD_2015}","mouseover",function(sym,e){sym.getSymbol("bot_AD_2015").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_AD_2015}","mouseout",function(sym,e){sym.getSymbol("bot_AD_2015").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_CP_2015}","mouseover",function(sym,e){sym.getSymbol("bot_CP_2015").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_CP_2015}","mouseout",function(sym,e){sym.getSymbol("bot_CP_2015").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_O_2015}","mouseover",function(sym,e){sym.getSymbol("bot_O_2015").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_O_2015}","mouseout",function(sym,e){sym.getSymbol("bot_O_2015").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_AD_2016}","mouseover",function(sym,e){sym.getSymbol("bot_AD_2016").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_AD_2016}","mouseout",function(sym,e){sym.getSymbol("bot_AD_2016").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_CP_2016}","mouseover",function(sym,e){sym.getSymbol("bot_CP_2016").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_CP_2016}","mouseout",function(sym,e){sym.getSymbol("bot_CP_2016").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_O_2016}","mouseover",function(sym,e){sym.getSymbol("bot_O_2016").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_O_2016}","mouseout",function(sym,e){sym.getSymbol("bot_O_2016").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_AD_2017}","mouseover",function(sym,e){sym.getSymbol("bot_AD_2017").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_AD_2017}","mouseout",function(sym,e){sym.getSymbol("bot_AD_2017").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_CP_2017}","mouseover",function(sym,e){sym.getSymbol("bot_CP_2017").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_CP_2017}","mouseout",function(sym,e){sym.getSymbol("bot_CP_2017").stop("Off");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_O_2017}","mouseover",function(sym,e){sym.getSymbol("bot_O_2017").stop("On");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bot_O_2017}","mouseout",function(sym,e){sym.getSymbol("bot_O_2017").stop("Off");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'bot_AD_2012'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_AD_2012");
+//Edge symbol end:'bot_AD_2012'
+
+//=========================================================
+
+//Edge symbol: 'bot_AD_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_CP_2012");
+//Edge symbol end:'bot_CP_2012'
+
+//=========================================================
+
+//Edge symbol: 'bot_CP_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_O_2012");
+//Edge symbol end:'bot_O_2012'
+
+//=========================================================
+
+//Edge symbol: 'bot_AD_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_AD_2013");
+//Edge symbol end:'bot_AD_2013'
+
+//=========================================================
+
+//Edge symbol: 'bot_CP_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_CP_2013");
+//Edge symbol end:'bot_CP_2013'
+
+//=========================================================
+
+//Edge symbol: 'bot_O_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_O_2013");
+//Edge symbol end:'bot_O_2013'
+
+//=========================================================
+
+//Edge symbol: 'bot_AD_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_AD_2014");
+//Edge symbol end:'bot_AD_2014'
+
+//=========================================================
+
+//Edge symbol: 'bot_CP_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_CP_2014");
+//Edge symbol end:'bot_CP_2014'
+
+//=========================================================
+
+//Edge symbol: 'bot_O_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_O_2014");
+//Edge symbol end:'bot_O_2014'
+
+//=========================================================
+
+//Edge symbol: 'bot_AD_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_AD_2015");
+//Edge symbol end:'bot_AD_2015'
+
+//=========================================================
+
+//Edge symbol: 'bot_CP_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_CP_2015");
+//Edge symbol end:'bot_CP_2015'
+
+//=========================================================
+
+//Edge symbol: 'bot_O_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_O_2015");
+//Edge symbol end:'bot_O_2015'
+
+//=========================================================
+
+//Edge symbol: 'bot_AD_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_AD_2016");
+//Edge symbol end:'bot_AD_2016'
+
+//=========================================================
+
+//Edge symbol: 'bot_CP_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_CP_2016");
+//Edge symbol end:'bot_CP_2016'
+
+//=========================================================
+
+//Edge symbol: 'bot_O_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_O_2016");
+//Edge symbol end:'bot_O_2016'
+
+//=========================================================
+
+//Edge symbol: 'bot_AD_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_AD_2017");
+//Edge symbol end:'bot_AD_2017'
+
+//=========================================================
+
+//Edge symbol: 'bot_CP_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_CP_2017");
+//Edge symbol end:'bot_CP_2017'
+
+//=========================================================
+
+//Edge symbol: 'bot_O_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("bot_O_2017");
+//Edge symbol end:'bot_O_2017'
+})(jQuery,AdobeEdge,"EDGE-11151096");
